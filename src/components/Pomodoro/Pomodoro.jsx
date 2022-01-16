a4049f17d18fe5f3a7c6b22c2e04a8c2a173c9dc
Andrey <mazurovandre@gmail.com>
2022-01-16 22:21:55 +0300

    Added SoundCloud Plugin

---
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SoundWidget from "../SoundCloud/SoundWidget";
+
+const Pomodoro = () => {
+    return (
+        <div>
+
+            <SoundWidget/>
+        </div>
+    );
+};
+
+export default Pomodoro;
